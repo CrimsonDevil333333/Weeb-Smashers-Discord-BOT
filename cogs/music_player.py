848288ecb1954d1829c554224e12467b966b0dc4
Satyaa Goyal <49252920+CrimsonDevil333333@users.noreply.github.com>
2020-07-29 01:59:59 +0530

    Fixed more voice channel bugs

--- cogs/music_player.py
+++ cogs/music_player.py
@@ -3,7 +3,7 @@ import youtube_dl
 import discord
 from discord.ext import commands
 import os
-"""
+
 try:
     if not discord.opus.is_loaded():
         discord.opus.load_opus('libopus.so')
@@ -12,7 +12,7 @@ try:
         print("already there")
 except:
     print("erorr came maybe its allready loaded or file is not here ")
-"""
+
 # Suppress noise about console usage from errors
 youtube_dl.utils.bug_reports_message = lambda: ''
 
